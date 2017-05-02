79534ae1b1b426dee05344e8d1b52e410bd688ef
amanchandra333 <amanchandra333@gmail.com>
2017-05-02 23:08:08 +0530

    added function for adding waypoint

--- include/ark_llp/go2goal.h
+++ include/ark_llp/go2goal.h
@@ -1,9 +1,16 @@
-#include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <sstream>
+#ifndef __GO2GOAL_H
+#define __GO2GOAL_H
+#include "std_msgs/String.h"
 
 class Go2Goal
 {
+private:
+	void comCb(const std_msgs::StringConstPtr str);
 public:
-  void set_dest();
+	void set_dest(double x, double y, double z, double theta);
+	void add_dest(double x, double y, double z, double theta);
+	void false_dest(double x, double y, double z, double theta);
+
 };
+
+#endif
